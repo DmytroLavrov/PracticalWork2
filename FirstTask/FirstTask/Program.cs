d8435383362f92e2deb9708e6156ef09a9075e1d
DmytroLavrov <112572226+DmytroLavrov@users.noreply.github.com>
2023-02-22 12:11:41 +0200

    Add files via upload

---
+++ FirstTask/FirstTask/Program.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FirstTask
+{
+    class Student
+    {
+        double arithmetic;
+        int pointN1;
+        int pointN2;
+        int pointN3;
+        string name;
+        public double Arithmetic
+        {
+            get { return arithmetic; }
+            set { arithmetic = value; }
+        }
+        public int PointN1
+        {
+            get { return pointN1; }
+            set { pointN1 = value; }
+        }
+        public int PointN2
+        {
+            get { return pointN2; }
+            set { pointN2 = value; }
+        }
+        public int PointN3
+        {
+            get { return pointN3; }
+            set { pointN3 = value; }
+        }
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public Student() { }
+        public Student(double arithmetic, int pointN1, int pointN2, int pointN3, string name)
+        {
+            Arithmetic = arithmetic;
+            PointN1 = pointN1;
+            PointN2 = pointN2;
+            PointN3 = pointN3;
+            Name = name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            string way = @"/Users/dmytrolavrov/Projects/PracticalWork2/FirstTask/СписокСтудентів.txt";
+            string[] list = File.ReadAllLines(way);
+            List<Student> students = new List<Student>();
+            List<string> ways = new List<string>();
+            foreach (var row in list)
+            {
+                ways.AddRange(row.Split(' '));
+                students.Add(new Student
+                {
+                    Name = ways[0],
+                    PointN1 = Convert.ToInt32(ways[1]),
+                    PointN2 = Convert.ToInt32(ways[2]),
+                    PointN3 = Convert.ToInt32(ways[3]),
+                    Arithmetic = (Convert.ToInt32(ways[1]) + Convert.ToInt32(ways[2]) + Convert.ToInt32(ways[3])) / 3,
+                });
+                ways.Clear();
+            }
+            foreach (Student student in students)
+            {
+                Console.OutputEncoding = System.Text.Encoding.Default;
+                Console.WriteLine($"Ім'я: {student.Name} \nСередній бал: {student.Arithmetic} \n");
+            }
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Список студентів за середнім балом:");
+            students.Sort((student1, student2) => student2.Arithmetic.CompareTo(student1.Arithmetic));
+            double ArithmeticMean = students.Average(s => s.Arithmetic);
+            List<Student> filteredStudents = students.Where(s => s.Arithmetic >= ArithmeticMean).ToList();
+            Console.WriteLine($"Середній бал групи: {ArithmeticMean} \n");
+            foreach (Student student in students)
+            {
+                Console.OutputEncoding = System.Text.Encoding.Default;
+                Console.WriteLine($"Ім'я: {student.Name} \nОцінки: {student.PointN1}, {student.PointN2}, {student.PointN3} \nCередній бал студента: {student.Arithmetic} \n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SecondTask/SecondTask/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SecondTask
+{
+    class Worker
+    {
+        int salary;
+        int workexperience;
+        string name = "";
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int WorkExperience
+        {
+            get { return workexperience; }
+            set { workexperience = value; }
+        }
+        public int Salary
+        {
+            get { return salary; }
+            set { salary = value; }
+        }
+        public Worker() { }
+        public Worker(int salary, int workexperience, string name)
+        {
+            Salary = salary;
+            WorkExperience = workexperience;
+            Name = name;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            string way = @"/Users/dmytrolavrov/Projects/PracticalWork2/SecondTask/SecondTask/СписокРобітників.txt";
+            string[] list = File.ReadAllLines(way);
+            List<Worker> workers = new List<Worker>();
+            List<string> type = new List<string>();
+            foreach (var row in list)
+            {
+                type.AddRange(row.Split(' '));
+                workers.Add(new Worker
+                {
+                    Name = type[0],
+                    WorkExperience = Convert.ToInt32(type[1]),
+                    Salary = Convert.ToInt32(type[2]),
+                });
+                type.Clear();
+            }
+            foreach (Worker worker in workers)
+            {
+                Console.WriteLine($"Ім'я: {worker.Name} \nРоки досвіду: {worker.WorkExperience} \nЗарплата: {worker.Salary} \n");
+            }
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Список співробітників за роками досвіду:");
+            workers.Sort((worker1, worker2) => worker2.WorkExperience.CompareTo(worker1.WorkExperience));
+            foreach (Worker worker in workers)
+            {
+                Console.WriteLine($"Ім'я: {worker.Name} \nРоки досвіду: {worker.WorkExperience} \n");
+            }
+
+            Console.OutputEncoding = System.Text.Encoding.Default;
+            Console.WriteLine("Список співробітників за зарплатою:");
+            workers.Sort((worker1, worker2) => worker2.Salary.CompareTo(worker1.Salary));
+            foreach (Worker worker in workers)
+            {
+                Console.WriteLine($"Ім'я: {worker.Name} \nЗарплата: {worker.Salary} \n");
+            }
+            Console.ReadKey();
+        }
+    }
+}
